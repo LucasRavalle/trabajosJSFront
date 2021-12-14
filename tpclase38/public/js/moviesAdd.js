81156969fbf5f0a453d728db64286ce55ba5c5c8
LucasRavalle <ravallelucas@gmail.com>
2021-12-14 00:53:23 -0300

    microdesafio paso 2

--- tpclase38/public/js/moviesAdd.js
+++ tpclase38/public/js/moviesAdd.js
@@ -14,4 +14,46 @@ window.onload = function(){
         titulo.style.color = 'black'
     })
 
+    let input = document.querySelector("#titulo")
+    let estadoSecreto= 0
+
+    const key = (key)=>{
+        switch(true){
+            case estadoSecreto=== 0 && key.toLowerCase()==="s": 
+                estadoSecreto++
+                break
+            case estadoSecreto=== 1 && key.toLowerCase()==="e": 
+                estadoSecreto++
+                break
+            case estadoSecreto=== 2 && key.toLowerCase()==="c": 
+                estadoSecreto++
+                break
+            case estadoSecreto=== 3 && key.toLowerCase()==="r": 
+                estadoSecreto++
+                break
+            case estadoSecreto=== 4 && key.toLowerCase()==="e": 
+                estadoSecreto++
+                break
+            case estadoSecreto=== 5 && key.toLowerCase()==="t": 
+                estadoSecreto++
+                break
+            case estadoSecreto=== 6 && key.toLowerCase()==="o": 
+                alert("Secreto magico") 
+                estadoSecreto=0
+                break
+            
+            default:
+                estadoSecreto=0
+                break
+        }
+    }
+
+
+    input.addEventListener("keypress",(event)=>{
+        console.log(event.key)
+        key(event.key)
+        
+
+    })
+
 }
